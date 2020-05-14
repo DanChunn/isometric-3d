de3be16276c57fab9811e47c888d167ae83a3200
Dan <DanielChunn@gmail.com>
2020-05-14 05:22:48 -0400

    adding util for enriching debug log

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -4,10 +4,8 @@ using System.Collections;
 [RequireComponent(typeof(CharacterController))]
 
 public class PlayerController : MonoBehaviour {
-
 	public float speed = 5.0F;
 	public float gravity = 15.0F;
-	
 	private Vector3 moveDirection = Vector3.zero;
 	public CharacterController controller;
 
@@ -19,12 +17,14 @@ public class PlayerController : MonoBehaviour {
 	{
 		if (controller.isGrounded) 
 		{
-			moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-			moveDirection = transform.TransformDirection(moveDirection);
+			moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")); //raw for unsmoothed input
 			moveDirection *= speed;
+			moveDirection = transform.TransformDirection(moveDirection);
 		}
-		
 		moveDirection.y -= gravity * Time.deltaTime; //apply gravity
 		controller.Move(moveDirection * Time.deltaTime);
+
+		//Debug.Log(EnrichLogUtil.Colorize(moveDirection.ToString(), Color.blue));
+		//Debug.Log(moveDirection.ToString());
 	}
 }
---
+++ Assets/Scripts/Util/EnrichLogUtil.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class EnrichLogUtil
+{
+    static int logging_font_size = 24;
+    public static string Colorize(string text, Color color){
+        string size_tag_open = $"<size={logging_font_size}>";
+        string size_tag_close = $"</size>";
+    
+        string color_hex = ColorUtility.ToHtmlStringRGB(color);
+        string color_tag_open = $"<color=#{color_hex}>";
+        string color_tag_close = $"</color>";
+
+        string new_text = size_tag_open + color_tag_open + text + color_tag_close + size_tag_close;
+        return new_text;
+    }
+
+}
