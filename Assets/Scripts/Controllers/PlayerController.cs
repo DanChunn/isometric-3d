8b4e3ed38709a1d00f0c4e2701a13ded37a6a462
Dan <DanielChunn@gmail.com>
2020-05-14 03:55:46 -0400

    adding initial project settings and scripts

---
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CharacterController))]
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed = 5.0F;
+	public float gravity = 15.0F;
+	
+	private Vector3 moveDirection = Vector3.zero;
+	public CharacterController controller;
+
+	void Start(){
+		controller = GetComponent<CharacterController>();
+	}
+
+	void Update() 
+	{
+		moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+		moveDirection = transform.TransformDirection(moveDirection);
+		moveDirection *= speed;
+		// if (controller.isGrounded) 
+		// {
+		// 	moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+		// 	moveDirection = transform.TransformDirection(moveDirection);
+		// 	moveDirection *= speed;
+		// }
+		//moveDirection.y -= gravity * Time.deltaTime; //manual gravity
+		controller.Move(moveDirection * Time.deltaTime);
+	}
+}
